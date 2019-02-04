1af5f4d5792840a125b880507b9f5d0cadc17e29
ruslan kovalov <kovalov.ruslan@gmail.com>
2019-02-04 11:20:18 +0200

    Add ApiKey Authenticator

---
+++ src/Security/ApiKeyAuthenticator.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Security;
+
+use App\Entity\User;
+use Doctrine\Common\Persistence\ManagerRegistry;
+use Ramsey\Uuid\Uuid;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
+use Symfony\Component\Security\Guard\AbstractGuardAuthenticator;
+
+class ApiKeyAuthenticator extends AbstractGuardAuthenticator
+{
+    /**
+     * @var ManagerRegistry
+     */
+    private $doctrine;
+
+    public function __construct(ManagerRegistry $doctrine)
+    {
+        $this->doctrine = $doctrine;
+    }
+
+    public function supports(Request $request)
+    {
+        return $request->headers->has('x-api-key');
+    }
+
+    public function getCredentials(Request $request)
+    {
+        return $request->headers->get('x-api-key');
+    }
+
+    public function getUser($credentials, UserProviderInterface $userProvider)
+    {
+        $apiKey = Uuid::fromString($credentials);
+
+        return $this->doctrine->getRepository(User::class)->findBy(['apiKey' => $apiKey]);
+    }
+
+    public function checkCredentials($credentials, UserInterface $user)
+    {
+        return true;
+    }
+
+    public function onAuthenticationFailure(Request $request, AuthenticationException $exception)
+    {
+        $data = [
+            'message' => strtr($exception->getMessageKey(), $exception->getMessageData()),
+        ];
+
+        return new JsonResponse($data, Response::HTTP_FORBIDDEN);
+    }
+
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token, $providerKey)
+    {
+        return null;
+    }
+
+    public function start(Request $request, AuthenticationException $authException = null)
+    {
+        $data = [
+            'message' => 'Authentication Required',
+        ];
+
+        return new JsonResponse($data, Response::HTTP_UNAUTHORIZED);
+    }
+
+    public function supportsRememberMe()
+    {
+        return false;
+    }
+}
